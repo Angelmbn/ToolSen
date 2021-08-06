28e5ae69d4f31049778a8d7dffcc470e8e84cfeb
Daniel Brito <87045417+danielpzbt@users.noreply.github.com>
2021-08-06 10:38:42 -0500

    Add files via upload

---
+++ web/partials/pqrsf.php
@@ -0,0 +1,59 @@
+    <!-- contact section -->
+    <section class="contact_section ">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="detail-box">
+                        <div class="heading_container">
+                            <h3>
+                                Contáctanos
+                            </h3>
+                            <p>
+                                Reporta cualquier incidente, irregularidad o felicitación respecto al funcionamiento de la página.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="contact-form">
+                        <h3>
+                            PQRSF
+                        </h3>
+                        <br>
+                        <form action="">
+                            <div class="row">
+                                <div class="col-md-auto">
+                                    <div>
+                                        <label>Tipo de PQRSF</label>
+                                    </div>
+                                </div>
+                                <div class="col-md-7">
+                                    <div>
+                                        <select name="tipo-pqrsf" id="">
+                                            <option value="" selected>Selecciona...</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <input type="text" placeholder="Nombres"/>
+                                <input type="text" placeholder="Apellidos"/>
+                            </div>
+                            <div>
+                                <input type="email" placeholder="Correo electrónico"/>
+                            </div>
+                            <div>
+                                <input type="text" placeholder="Observación" class="input_message" />
+                            </div>
+                            <div class="d-flex justify-content-center">
+                                <button type="submit" class="btn_on-hover">
+                                    Enviar
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end contact section -->
