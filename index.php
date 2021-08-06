400cf0c1d1c3d84a6715f5cd83cfc49592a2b141
Daniel Brito <87045417+danielpzbt@users.noreply.github.com>
2021-08-06 10:39:25 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+    include_once "web/partials/head.php";
+
+    echo "<body>";
+        echo "<div class='hero_area'>";
+
+            include_once "web/partials/header.php";
+
+            include_once "web/partials/slider.php";
+
+        echo "</div>";
+
+        include_once "web/partials/special.php";
+
+        include_once "web/partials/about.php";
+
+        include_once "web/partials/program.php";
+
+        include_once "web/partials/login.php";
+
+        include_once "web/partials/new.php";
+
+        include_once "web/partials/opinion.php";
+
+        include_once "web/partials/pqrsf.php";
+
+        include_once "web/partials/footer.php";
+
+        include_once "web/partials/script.php";
+
+    echo "</body>";
+
+    echo "</html>";
+
+?>
